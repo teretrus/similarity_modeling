854fc457a9ca9499f398a0668aaf4d0c2d11d882
Florian Schober <f.schober@live.com>
2014-10-14 15:33:43 +0200

    updated renaming-script to meet new naming-conventions. updated words of person 1. updated a few phonems
    Signed-off-by: Florian Schober <f.schober@live.com>

--- task1/audio samples/renaming.ps1
+++ task1/audio samples/renaming.ps1
@@ -1,14 +1,14 @@
 ﻿Param(
-  [string]$title
+  [string]$pattern
 )
 
-$pattern = [String]::Format("{0}--*", $title);
-$files = ls ($pattern);
+[regex]$regex = "^(?<example>[0-9]+)[-](?<person>[0-9]+)[-](?<gender>[fm])[-](?<age>[0-9]+)[-][-](?<code>[0-9]+)(?<ext>[.]wav)$";
 
-$file_count = ($files | measure).Count;
+$files = ls ($pattern) | where { $regex.IsMatch($_.Name) };
+[uint]$file_count = ($files | measure).Count;
 
 if ($file_count -eq 0) {
-    write-error [String]::Format("No file found that matches the pattern '{0}'", $pattern)
+    write-error ([String]::Format("No file found that matches the pattern '{0}'", $pattern));
 }
 else {
     write-host "";
@@ -16,9 +16,9 @@ else {
     write-host "";
 
     $files | foreach-object {
-        $number = $_.BaseName.Substring($pattern.Length-1);
-
-        $new_name = [String]::Format("E{0}-{1}{2}",$number, $title, $_.Extension);
+        
+        $match = $regex.Match($_.Name);
+        $new_name = [String]::Format("{0}-{1}-{2}-{3}-{4}-{5}{6}", "word", "E" + $match.Groups['code'], $match.Groups['example'], $match.Groups['person'], $match.Groups['gender'], $match.Groups['age'], $match.Groups['ext']);
 
         write-host ([String]::Format("  {0} --> {1}", $_.Name, $new_name)) -ForegroundColor Gray;
 
